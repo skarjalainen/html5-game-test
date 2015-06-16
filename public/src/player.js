3b7725b510ccc7e2cfc71f5a6b45fccd00c31d54
Sami Karjalainen <sami.karjalainen@sc5.io>
2015-06-16 22:51:31 +0300

    Multilpayer platformer logic

---
+++ public/src/player.js
@@ -0,0 +1,38 @@
+require([], function () { 
+  Q.Sprite.extend('Player', {
+    init: function (p) {
+      this._super(p, {
+        sheet: 'player',
+        jumpSpeed: -600
+      });
+ 
+      this.add('2d, platformerControls, animation');
+    },
+    step: function (dt) {
+      /*if (Q.inputs['up']) {
+        this.p.vy = -200;
+      } else if (Q.inputs['down']) {
+        this.p.vy = 200;
+      } else if (!Q.inputs['down'] && !Q.inputs['up']) {
+        this.p.vy = 0;
+      }*/
+      this.p.socket.emit('update', { playerId: this.p.playerId, x: this.p.x, y: this.p.y, sheet: this.p.sheet });
+    }
+  });
+
+  Q.Sprite.extend('Actor', {
+    init: function (p) {
+      this._super(p, {
+        update: true
+      });
+   
+      var temp = this;
+      setInterval(function () {
+        if (!temp.p.update) {
+          temp.destroy();
+        }
+        temp.p.update = false;
+      }, 3000);
+    }
+  });
+});
